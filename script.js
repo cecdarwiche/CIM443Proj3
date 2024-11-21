99c65a2af873011a027e0cf532e343bda7068bae
cecdarwiche <129707322+cecdarwiche@users.noreply.github.com>
2024-11-21 01:06:24 -0500

    test

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const suggestions = suggestSection.querySelectorAll('h3');
 
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(startVideo)
 
 function startVideo() {
